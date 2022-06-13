0eaf01b2b0156b0d063d403feaa940cbbf0afa3a
Brendon Bown <brendon.bown@gmail.com>
2022-06-12 19:30:31 -0600

    Added case expression

--- app/Main.hs
+++ app/Main.hs
@@ -214,9 +214,34 @@ eval val@(Bool _) = return val
 eval val@(Character _) = return val
 eval (Vector v) = Vector <$> mapM eval v
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+    do result <- eval pred
+       case result of
+           Bool True -> eval conseq
+           Bool False -> eval alt
+           _ -> throwError $ TypeMismatch "boolean" pred
+eval (List (Atom "cond" : conds)) = evalConds conds
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
+evalConds :: [LispVal] -> ThrowsError LispVal
+evalConds [] = return $ Bool False
+evalConds [List ((Atom "else") : exprs)] = evalExprList exprs
+evalConds (cond : conds) = evalCond cond conds
+
+evalCond :: LispVal -> [LispVal] -> ThrowsError LispVal
+evalCond (List (pred : conseqs)) rest = do
+    result <- eval pred
+    case result of
+        Bool True -> evalExprList conseqs
+        Bool False -> evalConds rest
+        _ -> throwError $ TypeMismatch "boolean" pred
+evalCond badArg _ = throwError $ BadSpecialForm "Invalid 'cond' branch" badArg
+
+evalExprList :: [LispVal] -> ThrowsError LispVal
+evalExprList [] = return $ Bool False
+evalExprList [expr] = eval expr
+evalExprList (expr : exprs) = eval expr >> evalExprList exprs
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
@@ -341,8 +366,8 @@ isBoolean (Bool _) = True
 isBoolean _ = False
 
 isPair :: LispVal -> Bool
-isPair (List (_:_)) = True
-isPair (DottedList _ _) = True
+isPair (List (_ : _)) = True
+isPair (DottedList (_ : _) _) = True
 isPair _ = False
 
 isNull :: LispVal -> Bool
@@ -372,6 +397,54 @@ isNumber _ = False
 isVector :: LispVal -> Bool
 isVector (Vector _) = True
 isVector _ = False
+
+-- ### LIST HANDLING PRIMITIVES
+
+listHandling :: PrimitiveDict
+listHandling = [("car", car),
+                ("cdr", cdr),
+                ("cons", cons)]
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : xs)] = return x
+car [DottedList (x : xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x : xs)] = return $ List xs
+cdr [DottedList [_] x] = return x
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []] = return $ List [x1]
+cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+-- ### EQUIVALENCE
+
+equivalence :: PrimitiveDict
+equivalence = [("eq?", eqv),
+               ("eqv?", eqv)]
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [Bool arg1, Bool arg2] = return $ Bool $ arg1 == arg2
+eqv [Number arg1, Number arg2] = return $ Bool $ arg1 == arg2
+eqv [String arg1, String arg2] = return $ Bool $ arg1 == arg2
+eqv [Character arg1, Character arg2] = return $ Bool $ arg1 == arg2
+eqv [Atom arg1, Atom arg2] = return $ Bool $ arg1 == arg2
+eqv [DottedList xs x, DottedList ys y] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [List arg1, List arg2] = return $ Bool $ (length arg1 == length arg2) &&
+                                             all eqvPair (zip arg1 arg2)
+    where eqvPair (x1, x2) = case eqv [x1, x2] of
+                               Right (Bool val) -> val
+                               Left err -> False
+                               _ -> False
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
 
 -- ### SYMBOL HANDLING PRIMITIVES
 
